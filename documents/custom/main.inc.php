89c5f3922eeac3d3363355822a183f5f39c0811d
root <root@iconeo.es>
2015-01-28 17:56:59 +0100

    first commit

---
+++ documents/custom/main.inc.php
@@ -0,0 +1,4 @@
+<?php
+// Wrapper to include main into htdocs
+include_once '/home/web/d/dolibarr-vide/htdocs/main.inc.php';
+?>
---
+++ documents/custom/master.inc.php
@@ -0,0 +1,4 @@
+<?php
+// Wrapper to include master into htdocs
+include_once '/home/web/d/dolibarr-vide/htdocs/master.inc.php';
+?>
